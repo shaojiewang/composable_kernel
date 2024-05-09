2d4efa1c4b4dda8a2c8c3fe3e292f73184243f89
shaojiewang <wsjmessi@163.com>
2024-05-09 09:24:49 +0000

    add some test script

--- example/91_tile_program/fmha/script/benchmark_recogpt.sh
+++ example/91_tile_program/fmha/script/benchmark_recogpt.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 # TODO: run this script from CK root
+set -x
+
 BUILD=build
 EXE=$BUILD/bin/example_fmha_fwd
 VALID=0
 
-for hidden in 1536; do # 5120 2048 ; do
-for seq in 32668;do # 256 512 1024 2048; do
-for bs in 1;do # 2 4 8 12 16 32; do
+for hidden in 64 ; do
+for seq in 922; do
+for bs in 300; do
 for prec in "bf16" ; do
 for perm in 0 ; do
-for hdim in 128 ; do
-for tmask in 1 ; do
+for hdim in 32 ; do
+for tmask in 0 ; do
 
 nhead=$(($hidden / $hdim))     # follow fav2 setup
 $EXE -prec=$prec -b=$bs -h=$nhead -d=$hdim -s=$seq   -iperm=$perm -operm=$perm -mask=$tmask -v=$VALID ; sleep 3
